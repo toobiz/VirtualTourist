273eab7821c670eed459fef29b7dc7a03a481947
Mike Tubis <michal.tubis@weblify.co>
2016-03-11 13:49:44 +0100

    Fabric Build 0.0.1

--- Virtual Tourist/AppDelegate.swift
+++ Virtual Tourist/AppDelegate.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
+import Fabric
 import CoreData
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
         return true
     }
 
